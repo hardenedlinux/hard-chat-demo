6c3891089b32e46df1f2d56035990382b9423e6f
Roy Mu <NalaGinrut@hardenedlinux.org>
2024-05-25 19:50:21 +0900

    Fix download link

--- scripts/download-model.sh
+++ scripts/download-model.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -c https://huggingface.co/bartowski/Meta-Llama-3-8B-Instruct-GGUF-old/raw/main/Meta-Llama-3-8B-Instruct-Q4_K_S.gguf -O models/llama-3-8B-q4-k-m.gguf
+wget -c https://huggingface.co/bartowski/Meta-Llama-3-8B-Instruct-GGUF-old/resolve/main/Meta-Llama-3-8B-Instruct-Q4_K_S.gguf -O models/llama-3-8B-q4-k-m.gguf
