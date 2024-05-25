c69db7ad8305fd47700efa8ca5998f43dc16051b
Roy Mu <NalaGinrut@hardenedlinux.org>
2024-05-25 20:01:05 +0900

    Don't cache the example

--- run.py
+++ run.py
@@ -162,7 +162,7 @@ with gr.Blocks(theme='ParityError/Anime') as demo:
         inputs=textbox,
         outputs=[textbox, chatbot],
         fn=process_example,
-        cache_examples=True,
+        cache_examples=False,
     )
 
     textbox.submit(
